fc8c73a95c1ccb081cdfac1803d9fc10588e6809
fileho <jiri.filek@gmail.com>
2021-04-26 13:48:34 +0200

    Add project files.

---
+++ Game/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿
+namespace Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pbPlayer = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pbPlayer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbPlayer
+            // 
+            this.pbPlayer.BackColor = System.Drawing.Color.Blue;
+            this.pbPlayer.Location = new System.Drawing.Point(512, 506);
+            this.pbPlayer.Name = "pbPlayer";
+            this.pbPlayer.Size = new System.Drawing.Size(50, 50);
+            this.pbPlayer.TabIndex = 0;
+            this.pbPlayer.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 20;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1132, 594);
+            this.Controls.Add(this.pbPlayer);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pbPlayer)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbPlayer;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Game/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Game
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            player = new Player(pbPlayer, 8);
+        }
+
+        private Player player;
+        private int spawnCount = 20;
+        private List<Enemy> enemies = new List<Enemy>();
+        private Random rnd = new Random();
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.A)
+                player.left = true;
+            if (e.KeyCode == Keys.D)
+                player.right = true;
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.A)
+                player.left = false;
+            if (e.KeyCode == Keys.D)
+                player.right = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            player.Move();
+            SpawnEnemy();
+
+            MoveEnemies();
+
+            Collisions();
+        }
+
+        private void MoveEnemies()
+        {
+            List<Enemy> toRemove = new List<Enemy>();
+
+            foreach (var enemy in enemies)
+            {
+                if (enemy.Move() > Height)
+                {
+                    toRemove.Add(enemy);
+                }
+            }
+            
+
+
+            foreach (var enemy in toRemove)
+            {
+                Controls.Remove(enemy.Pb);
+                enemies.Remove(enemy);
+            }
+        }
+
+        private void SpawnEnemy()
+        {
+            --spawnCount;
+
+            if (spawnCount > 0)
+                return;
+
+            spawnCount = 20;
+
+            const int offset = 30;
+            const int enemySize = 10;
+
+            int x = rnd.Next(offset, Width - offset - enemySize);
+
+            enemies.Add(new Enemy(rnd.Next(4, 10), x, enemySize));
+
+            Controls.Add(enemies[enemies.Count - 1].Pb);
+
+            
+        }
+
+        private void Collisions()
+        {
+            foreach (var enemy in enemies)
+            {
+                if (enemy.Pb.Bounds.IntersectsWith(player.Pb.Bounds))
+                {
+                    Gameover();
+                }
+            }
+        }
+
+        private void Gameover()
+        {
+            timer1.Enabled = false;
+            MessageBox.Show("Gameover");
+        }
+    }
+
+    public class Player
+    {
+        public bool left = false;
+        public bool right = false;
+
+        private PictureBox pb;
+        private readonly int speed;
+
+        public Player(PictureBox pb, int speed)
+        {
+            this.pb = pb;
+            this.speed = speed;
+        }
+
+        public PictureBox Pb { get => pb; }
+
+        public void Move()
+        {
+            Point location = Pb.Location;
+
+            if (left)
+                location.X -= speed;
+            if (right)
+                location.X += speed;
+
+            Pb.Location = location;
+        }
+    }
+
+    public class Enemy
+    {
+        private PictureBox pb;
+        private int speed;
+
+        public Enemy(int speed, int x, int size)
+        {
+            pb = new PictureBox();
+            pb.Location = new Point(x, 0);
+            pb.Size = new Size(size, size);
+            pb.BackColor = Color.FromArgb(255, 0, 0);
+
+            this.speed = speed;
+        }
+
+        public PictureBox Pb
+        {
+            get => pb;
+        }
+
+        public int Move()
+        {
+            Point location = pb.Location;
+
+            location.Y += speed;
+            
+
+            pb.Location = location;
+
+            return location.Y;
+        }
+
+    }
+
+}
